85824a7eaaea1ce35beae4e6e44a4e606ce98ff4
Sai santhosh Vemula <138227541+vss17@users.noreply.github.com>
2025-09-26 13:44:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,43 @@
-import os
-from PIL import Image
+from dotenv import load_dotenv
 import streamlit as st
-from streamlit_option_menu import option_menu
-from gemini_utility import (
-    load_gemini_pro_model,
-    gemini_pro_response,
-    gemini_pro_vision_response,
-    embeddings_model_response,
-)
+from langchain_groq import ChatGroq
 
-# Get the working directory
-working_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Streamlit page configuration
+# load the env variables
+load_dotenv()
+
+# streamlit page setup
 st.set_page_config(
-    page_title="Bujji AI",
-    page_icon="🧠",
+    page_title="Chatbot",
+    page_icon="🤖",
     layout="centered",
 )
+st.title("💬 Generative AI Chatbot")
 
-# Sidebar menu
-with st.sidebar:
-    selected = option_menu(
-        "Bujji AI",
-        ["ChatBot", "Embed text", "Ask me anything"],
-        menu_icon="robot",
-        icons=["chat-dots-fill", "textarea-t", "patch-question-fill"],
-        default_index=0,
+# initiate chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# show chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# llm initiate
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0.0,
+)
+
+# input box
+user_prompt = st.chat_input("Ask Chatbot...")
+
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+    response = llm.invoke(
+        input = [{"role": "system", "content": "You are a helpful assistant"}, *st.session_state.chat_history]
     )
-
-# Function to translate roles for Streamlit chat UI
-def translate_role_for_streamlit(user_role):
-    return "assistant" if user_role == "model" else user_role
-
-
-# Chatbot page
-if selected == "ChatBot":
-    model = load_gemini_pro_model()
-
-    # Initialize chat session in Streamlit if not already present
-    if "chat_session" not in st.session_state:
-        st.session_state.chat_session = model.start_chat(history=[])
-
-    # Page title
-    st.title("🤖 Bujji")
-
-    # Display the chat history
-    for message in st.session_state.chat_session.history:
-        with st.chat_message(translate_role_for_streamlit(message.role)):
-            st.markdown(message.parts[0].text)
-
-    # User input for chatbot
-    user_prompt = st.chat_input("Ask Bujji...")
-    if user_prompt:
-        # Add user message to chat
-        st.chat_message("user").markdown(user_prompt)
-
-        # Send user input to Gemini-Pro model and get response
-        gemini_response = st.session_state.chat_session.send_message(user_prompt)
-
-        # Display assistant's response
-        with st.chat_message("assistant"):
-            st.markdown(gemini_response.text)
-
-# Embed Text page
-if selected == "Embed text":
-    st.title("🔡 Embed Text")
-
-    # User input for text embedding
-    user_prompt = st.text_area(label="", placeholder="Enter the text to get embeddings")
-    if st.button("Get Embeddings"):
-        if user_prompt.strip():
-            response = embeddings_model_response(user_prompt)
-            st.markdown(response)
-        else:
-            st.warning("Please enter some text to get embeddings.")
-
-# Ask Me Anything page
-if selected == "Ask me anything":
-    st.title("❓ Ask me a question")
-
-    # User input for general query
-    user_prompt = st.text_area(label="", placeholder="Ask me anything...")
-    if st.button("Get Response"):
-        if user_prompt.strip():
-            response = gemini_pro_response(user_prompt)
-            st.markdown(response)
-        else:
-            st.warning("Please enter a question to get a response.")
-          
+    assistant_response = response.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
